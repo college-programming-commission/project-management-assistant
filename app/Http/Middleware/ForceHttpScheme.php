7f2a7cb18602bcdc9bf1c8b8b91251d0e3a7fe62
arakviel <insider.arakviel@gmail.com>
2025-10-18 22:11:12 +0300

    docker prod

--- app/Http/Middleware/ForceHttpScheme.php
+++ app/Http/Middleware/ForceHttpScheme.php
@@ -10,14 +10,14 @@ class ForceHttpScheme
 {
     public function handle(Request $request, Closure $next): Response
     {
-        // Примусово встановити HTTP схему для всіх запитів
-        $request->server->set('HTTPS', 'off');
-        $request->server->set('HTTP_X_FORWARDED_PROTO', 'http');
-        $request->server->set('HTTP_X_FORWARDED_SSL', 'off');
-        $request->server->remove('HTTPS');
-        
-        // Встановити схему запиту на HTTP
-        $request->setTrustedProxies(['0.0.0.0/0'], Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+        // Довіряти всім проксі для правильного визначення протоколу та хосту
+        $request->setTrustedProxies(
+            ['*'],
+            Request::HEADER_X_FORWARDED_FOR | 
+            Request::HEADER_X_FORWARDED_HOST | 
+            Request::HEADER_X_FORWARDED_PORT | 
+            Request::HEADER_X_FORWARDED_PROTO
+        );
 
         return $next($request);
     }
