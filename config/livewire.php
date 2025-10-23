cacbef081398f1d87e1096d36448e9a060f234d3
Vereshchahin Olexander <insider.arakviel@gmail.com>
2025-10-23 10:09:04 +0300

    fix minio

--- config/livewire.php
+++ config/livewire.php
@@ -64,23 +64,17 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+        'disk' => 'livewire-s3',  // Окремий диск для Livewire
+        'rules' => null,          // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => null,      // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,     // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes' => [      // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
         'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
-        's3' => [
-            'bucket' => env('AWS_BUCKET'),
-            'endpoint' => env('LIVEWIRE_S3_ENDPOINT', env('AWS_ENDPOINT')),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'options' => [],
-        ],
     ],
 
     /*
