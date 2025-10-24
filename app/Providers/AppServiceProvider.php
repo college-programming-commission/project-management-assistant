9d8aac8dd39535fdb48c7261bbd3966b5a938858
arakviel <insider.arakviel@gmail.com>
2025-10-24 14:12:06 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-        Model::shouldBeStrict();
+        // Model::shouldBeStrict();
 
         Paginator::useTailwind();
 
